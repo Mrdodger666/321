9751a0876bbc318e14ab91e2732e458bf7134adb
Jeremi <yater1466@gmail.com>
2024-02-15 14:27:44 +0100

    Juz nie alfa teraz sigma

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,8 @@ public class Main {
             System.out.println("i = " + i);
 
             System.out.println(Może i jesteś sigma ale ja jestem większym);
+
+            System.out.println(Już nie alfta teraz Sigma);
         }
     }
 }
